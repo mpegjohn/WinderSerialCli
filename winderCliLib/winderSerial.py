ae31edb7aa4ccecfba4a6283cb9d6feb3f3bbe76
mpegjohn <john_wilkinson3@sky.com>
2017-01-31 10:37:27 +0000

    Added conmments to the library

--- winderCliLib/winderSerial.py
+++ winderCliLib/winderSerial.py
@@ -1,3 +1,7 @@
+"""
+All classes that relate to the arduino winder serial interface.
+"""
+
 import struct
 import serial
 import serial.tools.list_ports
@@ -5,10 +9,18 @@ import serial.tools.list_ports
 
 class Ifc(object):
     def __init__(self, selected_port=None):
+        """Construvtor
+        Args:
+             selected_port (string) - Optional port selelection.
+        """
         self._selected_port = selected_port
         return
 
     def get_all_ports(self):
+        """Get all the serial ports on this machine
+        Returns:
+            ports {list) - List of all port names
+        """
         all_ports = serial.tools.list_ports.comports()
 
         ports = []
@@ -20,29 +32,39 @@ class Ifc(object):
 
     @property
     def selected_port(self):
+        """Sets the selected port."""
         return self._selected_port
 
     @selected_port.setter
     def selected_port(self, val):
+        """Gets the selected port."""
         self._selected_port = val
 
     def setup_serial(self):
+        """Sets the serial port up using the selected port."""
         self.ser = serial.Serial(self.selected_port)
         return
 
-    """ Writes a job to the arduino
-    sends 'SJ' for set job mode
-    WS[4 bytes] - wire size
-    SL[4 bytes] - spool length
-    TT[4 bytes] - number of turns
-    NL[1 byte ] -- Number of whole layers
-    LL[4 bytes ] -- Turns last layer
-    DN
-
-    """
-
     def write_job(self, wire_size, spool_length, turns, num_layers, last_turns):
+        """Writes a job to the arduino.
 
+         Sends data in the required sequence to setup a new winding job.
+         SJ = Setup Job
+         WS[4 bytes] - wire size
+         SL[4 bytes] - spool length
+         TT[4 bytes] - number of turns
+         NL[1 byte ] -- Number of whole layers
+         LL[4 bytes ] -- Turns last layer
+         DN - Done
+
+         Args:
+             wire_size (float) - The wire diameter in mm
+             spool_length (float) - The spool length in mm
+             turns (float) - Total number of turns
+             num_layers (int) - The number of layers
+             last_turns (float) - The number of turns on the last layer
+
+         """
         self.send_heading('SJ')
         self.send_heading('WS')
         self.send_float(wire_size)
@@ -57,7 +79,17 @@ class Ifc(object):
         self.send_heading('DN')
 
     def send_heading(self, heading):
+        """ Sends a two byte ascii heading to the arduino
 
+        Args:
+            heading (string) - The heading to send
+        Returns:
+            True if all sent OK
+        Raises:
+            TypeError - Non string type as argument
+            BufferError - Bytes sent diaparity or data disparity
+
+        """
         if not isinstance(heading, basestring):
             raise TypeError("Heading must be a string")
 
@@ -75,6 +107,16 @@ class Ifc(object):
         return True
 
     def send_byte(self, data):
+        """ Sends a byte of data to the arduino
+        Args:
+             data (int) - Data to send
+        Returns:
+            True if all OK
+        Raises:
+            TypeError - If data is not an int
+            ValueError - if value not within byte range of 0-255
+            BufferError - If data disparity
+        """
 
         try:
             int(data)
@@ -102,6 +144,20 @@ class Ifc(object):
         return True
 
     def send_float(self, data):
+        """ Sends a float to the arduino
+        The float is converted to a 4 byte ieee754 data block and
+        sent as a 4 byte bit of data.
+
+        Args:
+             data (float) - Data to send
+        Returns:
+            True if all OK
+        Raises:
+            TypeError - If data is not an float
+            ValueError - if value not within byte range of 0-255
+            BufferError - If data disparity
+
+        """
 
         try:
             float(data)
